f74f830772fcaff961ef64ef0d85d71cf00af546
Vishnu <vishnuramjawaharram@gmail.com>
2026-04-20 21:13:31 -0700

    4/20/2026

---
+++ Blind 75/search_in_rotated_sorted_array.py
@@ -0,0 +1,34 @@
+"""Problem 33 - Search in Rotated Sorted Array
+   Link - https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/solutions/6750367/video-using-part-of-input-array-to-compa-q291/
+   
+   Question - There is an integer array nums sorted in ascending order (with distinct values).
+              Prior to being passed to your function, nums is possibly left rotated at an unknown index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be left rotated by 3 indices and become [4,5,6,7,0,1,2].
+              Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+              You must write an algorithm with O(log n) runtime complexity."""
+
+
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left,right = 0,len(nums)-1
+        while left<=right:
+            mid = (left+right)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid]>= nums[left]:
+                if nums[left]<=target<=nums[mid]:
+                    right = mid-1
+                else:
+                    left = mid+1
+            else:
+                if nums[mid]<=target<=nums[right]:
+                    left = mid+1
+                else:
+                    right = mid-1
+        return -1
+
+        
