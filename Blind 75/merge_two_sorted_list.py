f8f505c2662f1ffc04f96baa28b88ce0fb60b201
Vishnu <vishnuramjawaharram@gmail.com>
2026-04-22 22:44:28 -0700

    4/22/2026

--- Blind 75/merge_two_sorted_list.py
+++ Blind 75/merge_two_sorted_list.py
@@ -5,12 +5,6 @@
               Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
               Return the head of the merged linked list."""
 
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-
 class Solution(object):
     def mergeTwoLists(self, list1, list2):
         dummy = ListNode()
